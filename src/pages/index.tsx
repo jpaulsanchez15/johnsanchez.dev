f4f8ce20b2fcf8a10d8a0c165c9d804630916b19
jpaulsanchez15 <jpsanchez1122@gmail.com>
2023-03-23 01:14:06 -0500

    styling on name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       <Navbar />
       <header>
         <h1 className="mt-4 py-2 text-center text-4xl font-extrabold tracking-tight text-white">
-          <span className="block bg-gradient-to-r from-white via-orange-400 to-orange-800 bg-clip-text text-8xl text-transparent">
+          <span className="block bg-gradient-to-r from-white via-orange-300 to-red-600 bg-clip-text text-8xl text-transparent">
             Hi, I&apos;m John Sanchez
           </span>
           <span className="block mt-3 text-6xl text-gray-300">
