1dca58c9d51593e2e3c6ab57bf3e840190c435c8
jpaulsanchez15 <jpsanchez1122@gmail.com>
2023-03-23 01:12:15 -0500

    starting index page styling and fun

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = Inter({ subsets: ['latin'] })
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
+
+export default App
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
-import Head from 'next/head'
+import { type NextPage } from "next";
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
 
-
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>John Sanchez | Home</title>
+      </Head>
+      <Navbar />
+      <header>
+        <h1 className="mt-4 py-2 text-center text-4xl font-extrabold tracking-tight text-white">
+          <span className="block bg-gradient-to-r from-white via-orange-400 to-orange-800 bg-clip-text text-8xl text-transparent">
+            Hi, I&apos;m John Sanchez
+          </span>
+          <span className="block mt-3 text-6xl text-gray-300">
+            Software Developer
+          </span>
+        </h1>
+      </header>
     </>
-  )
-}
+  );
+};
+
+export default Home;
