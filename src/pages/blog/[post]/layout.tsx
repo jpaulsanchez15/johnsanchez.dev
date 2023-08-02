f6986151cd5d9889b86d57b58969d8d505d45502
jpaulsanchez15 <jpsanchez1122@gmail.com>
2023-08-02 01:14:00 -0500

    more future planning, starting to add a footer to the blog pages

---
+++ src/pages/blog/[post]/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+/*
+ * Add footer here for the blog pages to show credit and what not.
+ */
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
